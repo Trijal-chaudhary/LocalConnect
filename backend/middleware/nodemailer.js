809a9534655bada8c77905624e8b3b316e5ba71c
Harsh Vardhan Chaudhary <harshvardhanchaudhary86@gmail.com>
2025-12-23 15:34:47 +0530

    fix the OTP functionality

--- backend/middleware/nodemailer.js
+++ backend/middleware/nodemailer.js
@@ -5,8 +5,8 @@ import nodemailer from "nodemailer";
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: 'harshvardhanchaudhary27@gmail.com',
     pass: 'ktezjjjrdnoucemj'
