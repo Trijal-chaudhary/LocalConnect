6f3b867b0d329e6c37738708ead30388f49cf056
Harsh Vardhan Chaudhary <harshvardhanchaudhary86@gmail.com>
2025-12-23 15:49:07 +0530

    fix nodemailer

--- backend/middleware/verification.js
+++ backend/middleware/verification.js
@@ -1,15 +1,23 @@
 import transporter from "./nodemailer.js";
 
 const verification = async (email, otp) => {
-  const info = await transporter.sendMail({
-    from: '"LocalConnect" <harshvardhanchaudhary27@gmail.com>',
-    to: email,
-    subject: "OTP",
-    text: "Hello harsh", // plain‑text body
-    html: `<b>OTP : ${otp}</b>`, // HTML body
-  });
+  try {
+    const info = await transporter.sendMail({
+      from: '"LocalConnect" <harshvardhanchaudhary27@gmail.com>',
+      to: email,
+      subject: "OTP - LocalConnect",
+      text: `Your OTP is ${otp}`,
+      html: `<b>OTP : ${otp}</b>`,
+
+    });
+    console.log("Message sent:", info.messageId);
+    return true;
+  } catch (error) {
+    console.error("OTP EMAIL ERROR:", error);
+    return false;
+  }
+
   // info();
-  console.log("Message sent:", info.messageId);
 }
 
 
