378b411e3906f7e2021d0a16c9f7e11283b44ae3
Harsh Vardhan Chaudhary <harshvardhanchaudhary86@gmail.com>
2026-02-07 12:22:51 +0530

    API keys of cloudinary,

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "dluw75knc",
-  api_key: "628132687229293",
-  api_secret: "y7t6rllZZYZ_tiWc4yuyImwqTYE",
+  cloud_name: "dluw5knc",
+  api_key: "628132687\29293",
+  api_secret: "y7t6rllZZYZ_tiWc4yImwqTYE",
 });
 
 module.exports = cloudinary;
